92603614e12e0d1b2865cd55acbbdaccf8e44f07
yasuhiro.tanaka <toytanaka@gmail.com>
2016-02-14 17:37:04 +0900

    テストリソースをクラスパス以下から検索するよう修正

--- testTraining/src/test/java/util/TestResourceManager.java
+++ testTraining/src/test/java/util/TestResourceManager.java
@@ -1,21 +1,31 @@
 package util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * テスト用リソースを管理する
  * @author yasuhiro.tanaka
  *
  */
 public class TestResourceManager {
-	/** テスト用リソースを配置するルートディレクトリのパス */
-	private static String tesrResourcesDir = "target/test-classes";
 
 	/**
-	 * テスト用リソースのパスを返す。
+	 * テスト用リソースをクラスパス配下から検索し絶対パスを返す。
 	 * テスト用リソース配置ルートディレクトリからの相対パスを引数に指定する。
 	 * @param resourcePathUnderTestResources
 	 * @return
+	 * @throws FileNotFoundException
 	 */
-	public static String getPath(String resourcePathUnderTestResources){
-		return tesrResourcesDir + "/" + resourcePathUnderTestResources;
+	public static String getPath(String resourcePathUnderTestResources) throws FileNotFoundException{
+		String[] classPath = System.getProperty("java.class.path").split(";");
+		for(String path : classPath){
+			File targetFile = new File(path + System.getProperty("file.separator") + resourcePathUnderTestResources);
+			if(targetFile.exists()){
+				return targetFile.getAbsolutePath();
+			}
+		}
+
+		throw new FileNotFoundException("対象パスが見つかりません。[ path = " + resourcePathUnderTestResources + " ]");
 	}
 }
